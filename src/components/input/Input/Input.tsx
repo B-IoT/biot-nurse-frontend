516cd20b42f53ee85200dd42b7f5af6e048d23dd
LudoHoff <ludo.hoffstetter@epfl.ch>
2021-09-28 14:45:24 +0200

    Change popup styles, fix popup animation and add firefox support

--- src/components/input/Input/Input.tsx
+++ src/components/input/Input/Input.tsx
@@ -19,25 +19,26 @@ export default function Input(props: InputProps) {
       className="search-bar"
       style={Object.assign({}, { width: width }, style)}
     >
-      <div className="search-mask" style={{ width: width }}>
-        <div className="search-shadow" style={{ width: width + 50 }} />
-        <div className="search-light" style={{ width: width + 50 }} />
-      </div>
       <div className="search-blur" style={{ width: width + 10 }}>
-        <input
-          type={isPassword ? 'password' : ''}
-          className="search-text font-axiforma-light text-blue text-small"
-          placeholder={defaultText}
-          onChange={(e) => {
-            if (setKeyword) {
-              return setKeyword(e.target.value);
-            }
-          }}
-          onKeyPress={(e) => (e.key === 'Enter' ? enterHandler() : null)}
-          style={{ width: width - 50 }}
-          data-testid="input"
-        />
+        <div className="search-mask" style={{ width: width }}>
+          <div className="search-shadow" style={{ width: width + 50 }} />
+          <div className="search-light" style={{ width: width + 50 }} />
+        </div>
       </div>
+
+      <input
+        type={isPassword ? 'password' : ''}
+        className="search-text font-axiforma-light text-blue text-small"
+        placeholder={defaultText}
+        onChange={(e) => {
+          if (setKeyword) {
+            return setKeyword(e.target.value);
+          }
+        }}
+        onKeyPress={(e) => (e.key === 'Enter' ? enterHandler() : null)}
+        style={{ width: width - 50 }}
+        data-testid="input"
+      />
     </div>
   );
 }
